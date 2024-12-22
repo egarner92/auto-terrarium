bc4ac0590ab7a0eddd55571ac48f5ece96eb180e
Eric Garner <ericgarner92@gmail.com>
2024-12-22 17:41:28 +0100

    add wifi connect

--- src/config.h
+++ src/config.h
@@ -1,1 +1,3 @@
 // All user-specific data
+const char *SSID = "Bbox-765231F0";
+const char *PASSWORD = "14bwgSJE67xxXmMwsm";
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include "config.h"
 
 // put function declarations here:
+void connectWifi();
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-
+  
   // Initialize serial communication
   Serial.begin(115200);
+
+  // Connect to WiFi
+  connectWifi();
+  delay(1000);
+
+  int result = myFunction(2, 3);
 }
 
 void loop() {
@@ -16,6 +24,17 @@ void loop() {
 }
 
 // put function definitions here:
+void connectWifi()
+{
+  Serial.print("Connecting to Wifi network: ");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  { delay(500); }
+  Serial.println("SUCCESS");
+}
+
 int myFunction(int x, int y) {
   return x + y;
 }
