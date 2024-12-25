d33f23d2d4944a93f0b8047fed12e4826fed0aa0
Eric Garner <ericgarner92@gmail.com>
2024-12-25 23:31:05 +0100

    minor improvements to pid class

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "PID.h"
 
-PID::PID(int &input, int &output, int &setpoint) : input(input), output(output), setpoint(setpoint), 
+PID::PID(int &input, int &output, int &setpoint, int freq) : input(input), output(output), setpoint(setpoint), freq(freq),
 kp(0), ki(0), kd(0), lastTime(millis()), integral(0.0), lastError(0.0)
 {
 }
@@ -17,26 +17,33 @@ void PID::compute()
 {
     // Calculate time since last compute
     unsigned long now = millis();
-    double dt = (now - lastTime) / 1000.00;
+    double dt = (now - lastTime) / 1000.0;
 
-    // Prevent division by zero
-    if (dt <= 0)
-        return; 
+    // Handle millis rollover
+    if (now < lastTime)
+    {
+        lastTime = now;
+        return;
+    }
 
-    // Compute proportional term
-    double error = setpoint - input;
-    double proportional = error;
+    // If time to update
+    if (dt >= 1.0/freq)
+    {
+        // Compute proportional term
+        double error = setpoint - input;
+        double proportional = kp * error;
 
-    // Increment integral term
-    integral += error * dt;
+        // Increment integral term
+        integral += error * dt;
 
-    // Compute derivative term
-    double derivative = (error - lastError) / dt;
+        // Compute derivative term
+        double derivative = (error - lastError) / dt;
 
-    // Set output
-    output = (kp * proportional) + (ki * integral) + (kd * derivative);
+        // Set output
+        output = proportional + (ki * integral) + (kd * derivative);
 
-    // Update state variables
-    lastTime = now;
-    lastError = error;
-}
+        // Update state variables
+        lastTime = now;
+        lastError = error;
+    }
+    }
--- src/PID.h
+++ src/PID.h
@@ -7,13 +7,14 @@ private:
     int& input;
     int& output;
     int& setpoint;
+    const int freq;
     double kp, ki, kd;
     unsigned long lastTime;
     double integral;
     double lastError;
 
 public:
-    PID(int &input, int &ouput, int &setpoint);
+    PID(int &input, int &ouput, int &setpoint, int freq);
     void setParams(const double kp, const double ki, const double kd);
     void compute();
 };
